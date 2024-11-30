8f921ca557ce1b769b97c1aa1cd934b972c7b34c
Luis-Mendieta <mendietafelizzola2@gmail.com>
2024-11-29 21:32:17 -0500

    correccion de nombres

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
  
 // Manejo de productos dinámico
 const products = [
-    { id: 1, name: "Laptop", price: 800, img: "pantalla-de-portatil.png" },
-    { id: 2, name: "Smartphone", price: 600, img: "smartphone.jpg" },
-    { id: 3, name: "Sofá", price: 300, img: "sofa.jpg" },
-    { id: 4, name: "Lámpara", price: 50, img: "lamp.jpg" },
-    { id: 5, name: "Bicicleta", price: 150, img: "bike.jpg" },
-    { id: 6, name: "Balón de fútbol", price: 30, img: "football.jpg" },
+    { id: 1, name: "Laptop", price: 800, img: "trafico-wed.png" },
+    { id: 2, name: "celular", price: 600, img: "chat.jpg" },
+    { id: 3, name: "audifonos", price: 300, img: "audifonos-inalambricos.jpg" },
+    { id: 4, name: "mesa de noche", price: 50, img: "mesita-de-noche.jpg" },
+    { id: 5, name: "lampara", price: 150, img: "lampara-de-escritorio.jpg" },
+    { id: 6, name: "tabla para picar de ", price: 30, img: "tabla-picar.jpg" },
   ];
  
   // Agregar producto al carrito
